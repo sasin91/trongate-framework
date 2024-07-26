02787470384179205465c2f4479f8ecc707445a1
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2024-07-26 15:35:22 +0200

    example impl.

--- modules/welcome/controllers/Welcome.php
+++ modules/welcome/controllers/Welcome.php
@@ -11,4 +11,26 @@ class Welcome extends Trongate {
 		$this->trongate_pages->display();
 	}
 
+    public function ehlo()
+    {
+        $this->module('cors');
+
+        /** @var Cors $cors */
+        $cors = $this->cors;
+
+        $cors->setup(
+            allowedOriginsString: 'http://localhost:3000',
+            allowedMethodsString: 'GET, OPTIONS',
+            allowedHeadersString: 'trongatetoken, X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding',
+            allowedCredentials: true
+        );
+
+        $cors->defineHeaders(origin: $_SERVER['HTTP_ORIGIN']);
+
+        //api_auth();
+
+        header('Content-Type: application/json');
+        echo json_encode(['body' => '<section><h1>EHLO</h1><p>how r u</p></section>']);
+    }
+
 }
