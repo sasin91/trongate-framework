e7c5cd2d1b7f65fe29c085dd15361c52aba93a2f
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2024-07-26 15:35:52 +0200

    Reverted CORS configs in config.php

--- config/config.php
+++ config/config.php
@@ -23,10 +23,3 @@ define('DEFAULT_CONTROLLER', 'Welcome');
 define('DEFAULT_METHOD', 'index');
 define('MODULE_ASSETS_TRIGGER', '_module');
 define('INTERCEPT_404', 'trongate_pages/attempt_display');
-
-//Cross-Origin Resource Sharing (CORS)
-define('CORS_ENABLED', env(key: 'CORS_ENABLED', default: false));
-define('CORS_ALLOWED_ORIGINS', env(key: 'CORS_ALLOWED_ORIGINS', default: 'http://localhost, http://localhost:3000'));
-define('CORS_ALLOWED_METHODS', env(key: 'CORS_ALLOWED_METHODS', default: 'GET, POST, PUT, DELETE, OPTIONS, PATCH'));
-define('CORS_ALLOWED_HEADERS', env(key: 'CORS_ALLOWED_HEADERS', default: 'Trongatetoken, X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding'));
-define('CORS_ALLOWED_CREDENTIALS', env(key: 'CORS_ALLOWED_CREDENTIALS', default: true));
