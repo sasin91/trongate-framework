b6252271da90482f7ff2cf1d1639d14c5bd391dd
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2025-03-07 12:41:10 +0100

    feat: added transition to demo pages

--- modules/welcome/views/flip.php
+++ modules/welcome/views/flip.php
@@ -1,4 +1,7 @@
 <section id="content">
-<h1>Hello world</h1>
-<button mx-get="/" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="flip-reverse">Flip back</button>
+  <h1>Hello world</h1>
+  <button mx-get="/" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="flip-reverse">Flip back</button>
+  <button mx-get="/" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="pop-reverse">Pop back</button>
+
+  <a href="/">back</a>
 </section>
--- modules/welcome/views/welcome.php
+++ modules/welcome/views/welcome.php
@@ -1,5 +1,12 @@
 <section id="content">
-<h1>welcome, welcome</h1>
-<button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="flip">Flip</button>
-<button onclick="window.location.reload();void(0);">Reload</button>
+  <h1>welcome, welcome</h1>
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="flip">Flip</button>
+  <button onclick="window.location.reload();void(0);">Reload</button>
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="pop">Pop</button>
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="push">Push</button>
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="flow">Flow</button>
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="slide-up">Slide up</button>
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="slide-down">Slide down</button>
+
+  <button mx-get="/welcome/flip" mx-target="#content" mx-swap="outerHTML" mx-select="#content" mx-transition="fade-slide">Fade & slide (custom one)</button>
 </section>
